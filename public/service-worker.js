267ab7619796b208f46a12a2d1f394d3f5f229f0
KevinPMyers <kmyers25@gmail.com>
2021-10-04 16:47:56 -0400

    needed to change file structure, serverworker.js was in directory, moved to public, files now cache but fetch request fails, error line 29

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,27 +3,11 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
-    "./css/style.css",
+    "./css/styles.css",
     "./js/idb.js",
     "./js/index.js"
 
 ]
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
-
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -32,6 +16,32 @@ self.addEventListener('install', function (e) {
         })
     )
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+
+    e.respondWith(
+        fetch(e.request).catch(function() {
+            return caches.match(e.request).then(function(response) {
+                if(response){
+                    return response
+                } else if(e.request.headers.get("accept").includes("text/html")) {
+                    return caches.match('/')
+                }
+            })
+        })
+        // caches.match(e.request).then(function (request) {
+        //     if (request) {
+        //         console.log('responding with cache : ' + e.request.url)
+        //         return request
+        //     } else {
+        //         console.log('file is not cached, fetching : ' + e.request.url)
+        //         return fetch(e.request)
+        //     }
+        // })
+    )
+})
+
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
