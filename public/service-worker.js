735070c2bac1485964b581546297b38b08804f49
KevinPMyers <kmyers25@gmail.com>
2021-10-05 21:26:46 -0400

    errors resolved in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,12 @@ const APP_PREFIX = 'budget-tracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./css/styles.css",
     "./js/idb.js",
     "./js/index.js"
+    
 
 ]
 self.addEventListener('install', function (e) {
@@ -15,6 +17,7 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', function (e) {
